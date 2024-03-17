2162a5fcc28337b4f782ed65e167d118417498bf
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-17 03:37:31 +0000

    skapat nya recipe componenter

---
+++ RecipeAPI/angular-frontend/src/app/recipe-component/recipe-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-component.component.html',
+  styleUrl: './recipe-component.component.scss'
+})
+export class RecipeComponentComponent {
+
+}
