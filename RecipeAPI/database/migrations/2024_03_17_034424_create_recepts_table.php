a9a10f771f5381d0ff8ee750ba9a1fb81bb74d3c
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-17 16:01:51 +0000

    skapat modeller för att skapa recept listor och table

--- RecipeAPI/database/migrations/2024_03_17_034424_create_recepts_table.php
+++ RecipeAPI/database/migrations/2024_03_17_034424_create_recepts_table.php
@@ -4,28 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+
+class CreateRecipesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('recipes', function (Blueprint $table) {
-            $table->id();
-            // Lägg till kolumner här, exempelvis:
+            $table->id('recipe_id');
+            $table->string('external_api_id')->nullable();
             $table->string('title');
-            $table->text('description');
-            // Lägg till fler kolumner efter behov
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('recepts');
+        Schema::dropIfExists('recipes');
     }
-};
+}
+
--- RecipeAPI/database/migrations/2024_03_17_035047_create_recipe_lists_table.php
+++ RecipeAPI/database/migrations/2024_03_17_035047_create_recipe_lists_table.php
@@ -4,24 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateRecipeListsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('recipe_lists', function (Blueprint $table) {
-            $table->id();
+            $table->id('list_id');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('restrict')->onUpdate('restrict');
+            $table->string('title');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('recipe_lists');
     }
-};
+}
