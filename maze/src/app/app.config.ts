1c762ce10b59137576e730f8bbf6552910530948
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-26 14:57:02 +0100

    Grundstruktur för angular

---
+++ maze/src/app/app.config.ts
@@ -0,0 +1,14 @@
+// Example Configuration Constants
+export const appConfig = {
+  apiBaseUrl: 'https://api.example.com/',
+  // Add more configuration constants as needed
+};
+
+
+
+
+// main.ts klar 
+
+// app.config.ts klar
+
+// app.routes.ts
