a59bda10d6f80b8c716ec6e7fe47c09f1b618b55
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-26 22:35:21 +0100

    tagit bort angular

--- maze/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router'; // Import the Routes type
-// import { HomeComponent } from './components/home/home.component';
-// import { LoginComponent } from './components/login/login.component';
-
-export const routes: Routes = [ // Explicitly type the array as Routes
-//   { path: 'login', component: LoginComponent },
-//   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
