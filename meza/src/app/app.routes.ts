a3a92ad70db52abd14842e4579868c7d69580fb8
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-29 00:42:40 +0100

    lagt till nya imports

--- meza/src/app/app.routes.ts
+++ meza/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { OmOssComponent } from './pages/om-oss/om-oss.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 
 export const routes: Routes = [
@@ -23,4 +25,9 @@ export const routes: Routes = [
     { path: 'layout', component: LayoutComponent },
     
     { path: 'footer', component: FooterComponent },
+    {path: 'search', component: RecipeSearchComponent},
+    {path: 'result' ,component:RecipeListComponent},
+
+    { path: '', redirectTo: '/search', pathMatch: 'full' }, // Omdirigering från rotvägen till söksidan
+
 ];
