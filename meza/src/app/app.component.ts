35196932a09b612618a3fe9df62b3e63f90aa1e1
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-28 17:15:02 +0100

    importerat in nav footer och signup komponenter i app.component.ts

--- meza/src/app/app.component.ts
+++ meza/src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './pages/navbar/navbar.component';
+
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavbarComponent,CommonModule,LoginComponent],
+  imports: [RouterOutlet, RouterLink, NavbarComponent,CommonModule,LoginComponent,FooterComponent,SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Meza';
+  
 }
 
