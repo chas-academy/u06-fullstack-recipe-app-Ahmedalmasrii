b7fe8a35c414d336dda85d229d0a79866883575d
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-28 17:09:35 +0100

    fixat så att man kan logga in via sign up

--- meza/src/app/sign-up/sign-up.component.ts
+++ meza/src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule , RouterLink],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  registerdetails: Register;
+  errorMessage: string = ''; // Ny egenskap för felmeddelanden
 
+
+  constructor(private auth: AuthService, private router: Router) { 
+    this.registerdetails = new Register();
+  }
+
+  register() {
+    this.auth.registerUser(this.registerdetails).subscribe({
+      next: (response) => {
+        console.log('User registered successfully', response);
+        alert('Your account has been created successfully. Please login.'); // Visa ett alert-meddelande
+
+        
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error('Registration error', error);
+        this.errorMessage='Registration failed. Please try again.'
+      }
+    });
+  }
+}
+
+export class Register {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation:string;
+
+  constructor() {
+    this.name ='';
+    this.email = '';
+    this.password = '';
+    this.password_confirmation= '';
+
+  }
 }
