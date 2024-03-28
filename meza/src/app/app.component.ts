33d00255d23ce0406d512aab4be2fe247591e3fd
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-28 03:13:46 +0100

    fixat till min app route och componenter

--- meza/src/app/app.component.ts
+++ meza/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './pages/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './pages/login/login.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, RouterLink, NavbarComponent,CommonModule,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'meza';
+  title = 'Meza';
 }
+
--- meza/src/app/app.routes.ts
+++ meza/src/app/app.routes.ts
@@ -2,23 +2,22 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { OmOssComponent } from './pages/om-oss/om-oss.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 export const routes: Routes = [
     {
-        path: '' , redirectTo : 'login' , pathMatch: 'full'
+        path: '', redirectTo: 'login', pathMatch: 'full'
     },
-    {
-        path:'login',
-        component: LoginComponent
-    }, 
-    {
-        path:'',
-        component:LayoutComponent,
-        children: [
-            {
-                path: 'home',
-                component: HomeComponent
-            }
-        ]
-        }
+
+    { path: 'login', component: LoginComponent },
+
+    { path: 'signup', component: SignUpComponent },
+
+    { path: 'home', component: HomeComponent },
+
+    { path: 'omoss', component: OmOssComponent },
+
+    { path: 'layout', component: LayoutComponent }
 ];
