47087d12524f0a1d820955b5b80012c85f035d46
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-29 00:41:10 +0100

    fixat till min inloggningsformilärs struktur med auth services

--- meza/src/app/pages/login/login.component.ts
+++ meza/src/app/pages/login/login.component.ts
@@ -1,45 +1,97 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { Router, RouterLink } from '@angular/router';
+
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [FormsModule, HttpClientModule,RouterLink],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.scss'
+// })
+// export class LoginComponent {
+//   loginObj: Login;
+//   constructor(private http: HttpClient, private router: Router) {
+//     this.loginObj = new Login();
+//   }
+//   onLogin() {
+//     this.authService.postLogin(this.loginObj)?.subscribe({
+//       next: (res) => {
+//         console.log(res);
+//         if (res.token) {
+//           localStorage.setItem('token', res.token);
+
+//           console.log(localStorage.getItem('token'));
+
+//           alert('Login Success');
+//           this.router.navigateByUrl('/home')
+
+//         } else {
+//           alert(res.message);
+//         }
+//       }
+//     })
+//     /* .subscribe((res: any) => {
+//       console.log(res.token)
+
+
+//     }); */
+//   }
+// }
+
+// export class Login {
+//   email: string;
+//   password: string;
+//   constructor() {
+//     this.email = '';
+//     this.password = '';
+//   }
+// }
+
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { Router, RouterLink } from '@angular/router';
+import { Login } from '../../models/Login.model';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, HttpClientModule,RouterLink],
+  imports: [FormsModule, HttpClientModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginObj: Login;
-  constructor(private http: HttpClient, private router: Router) {
+  showerrormessage:string='';
+  constructor(private router: Router, private authService: AuthService) {
     this.loginObj = new Login();
+    console.log(this.loginObj);
   }
   onLogin() {
-    this.http.post('http://127.0.0.1:8000/api/login', this.loginObj).subscribe((res: any) => {
-      console.log(res.token)
+    this.authService.login(this.loginObj)?.subscribe({
+      next: (res) => {
+        console.log(res);
+        if (res.token) {
+          localStorage.setItem('token', res.token);
 
-      if (res.token) {
-        localStorage.setItem('token', res.token);
+          console.log(localStorage.getItem('token'));
 
-        console.log(localStorage.getItem('token'));
+          alert('Login Success');
+          this.router.navigateByUrl('/home')
 
-        alert('Login Success');
-        this.router.navigateByUrl('/home')
-
-      } else {
-        alert(res.message);
+        } else {
+          alert(res.message);
+        }
+      },
+      error:(error)=>{
+        this.showerrormessage=error.error.message;
       }
-    });
-  }
-}
-
-export class Login {
-  email: string;
-  password: string;
-  constructor() {
-    this.email = '';
-    this.password = '';
+    })
+    
   }
 }
