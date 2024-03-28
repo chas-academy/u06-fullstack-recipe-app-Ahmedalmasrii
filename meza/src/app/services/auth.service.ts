039040e2828d12f473343113755146afce73bf25
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-29 00:42:28 +0100

    lagt till inlogings struktur i auth istället för componenten

--- meza/src/app/services/auth.service.ts
+++ meza/src/app/services/auth.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Login } from '../models/Login.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  login(loginObj:Login) {
+    if (!loginObj) return
+    console.log(loginObj);
+    return this.http.post<any>('http://127.0.0.1:8000/api/login', loginObj)
+  }
 
   registerUser(userData: any): Observable<any> {
-
     const url = 'http://127.0.0.1:8000/api/register';
     return this.http.post(url, userData);
   }
