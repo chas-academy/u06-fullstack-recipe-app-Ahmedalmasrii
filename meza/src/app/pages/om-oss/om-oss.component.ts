72c4ca6262eb60a1abf8676f6d6a137d9bdb0c6d
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-28 03:10:46 +0100

    skapat om oss component

---
+++ meza/src/app/pages/om-oss/om-oss.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-om-oss',
+  standalone: true,
+  imports: [],
+  templateUrl: './om-oss.component.html',
+  styleUrl: './om-oss.component.scss'
+})
+export class OmOssComponent {
+
+}
