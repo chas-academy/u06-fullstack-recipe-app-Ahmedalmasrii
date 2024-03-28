e4750843fcc767d2be60f3d19c789a04f83de295
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-28 17:11:08 +0100

    skapat en service interface

--- meza/src/app/services/auth.service.ts
+++ meza/src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registerUser(userData: any): Observable<any> {
+
+    const url = 'http://127.0.0.1:8000/api/register';
+    return this.http.post(url, userData);
+  }
 }
