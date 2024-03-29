3d5fe7c613d49f0af3207b805245a9c8ea141fa8
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-29 04:04:40 +0100

    ändrat url och ap key

--- meza/src/app/spoonacular.service.ts
+++ meza/src/app/spoonacular.service.ts
@@ -6,14 +6,14 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SpoonacularService {
-private apiKey:string='12f80af60ee846f49e6393fefe9189f5';
+private apiKey:string='aTVitVZcYYThxIY7kq5UYU6tNXqW2ktA';
 private recipeSubject:BehaviorSubject<any[]>=new BehaviorSubject<any[]>([]);
 recipes$:Observable<any[]>=this.recipeSubject.asObservable();
 
 
   constructor(private http:HttpClient) { }
   searchRecipes(query:string):void {
-    const url='https://api.spoonacular.com/recipes/complexSearch?query={query}';
+    const url = `https://api.apilayer.com/spoonacular/recipes/complexSearch?query=${query}`;
     const headers=new HttpHeaders().set('apikey',this.apiKey);
     
     this.http.get(url,{headers}).subscribe((data:any)=>{
