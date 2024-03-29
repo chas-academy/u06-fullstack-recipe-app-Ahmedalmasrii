6309ada504995b4bd632279d4db24e3a03e02abf
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-29 04:06:17 +0100

    importerat nya moduler och component

--- meza/src/app/app.config.ts
+++ meza/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync()]
