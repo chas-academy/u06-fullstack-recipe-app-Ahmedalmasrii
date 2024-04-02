4b2edd3e44495b0077b45e614044dcb951ae4878
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-02 03:15:54 +0200

    skapat dessert component

---
+++ meza/src/app/pages/desserts/desserts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desserts',
+  standalone: true,
+  imports: [],
+  templateUrl: './desserts.component.html',
+  styleUrl: './desserts.component.scss'
+})
+export class DessertsComponent {
+
+}
