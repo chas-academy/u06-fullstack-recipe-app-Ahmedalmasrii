34ccaf20c254cb11874266399b4ab7f90d8a0364
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-02 03:16:42 +0200

    fixat till min navbar

--- meza/src/app/pages/navbar/navbar.component.ts
+++ meza/src/app/pages/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -9,6 +9,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Token } from '@angular/compiler';
+import { AuthService } from '../../services/auth.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
@@ -24,15 +28,34 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     AsyncPipe,
     RouterLink,
     RouterLinkActive,
-    RouterOutlet
-  ]
+    RouterOutlet,
+  ],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+ 
   private breakpointObserver = inject(BreakpointObserver);
+  private authservice = inject(AuthService);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
+    ngOnInit(): void {
+    this.authservice.getTokenState().subscribe(state => {
+      this.loggedIn = state
+    })
+    }
+    
+  loggedIn: boolean = false
+
+  logout() {
+    const token = localStorage.getItem('token');
+    this.authservice.postLogout(token).subscribe((res) => {
+      localStorage.removeItem('token');
+      this.authservice.updateTokenState()
+      console.log(res);
+    });
+  }
 }
