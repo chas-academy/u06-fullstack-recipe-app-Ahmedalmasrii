f5ef85c628344742e76cbc5e2bad10f81ab550ba
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-04-03 17:20:23 +0200

    stylat om starter,main,dessert

--- meza/src/app/pages/starters/starters.component.ts
+++ meza/src/app/pages/starters/starters.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, input } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
 import { SearchComponent } from '../../search/search.component';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-mains',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent,RouterLink,RouterLinkActive],
   templateUrl: './starters.component.html',
   styleUrl: './starters.component.scss',
 })
@@ -16,9 +16,10 @@ export class StartersComponent {
   recieveRecipes(recipes: any[]) {
     this.recipes = recipes;
   }
+ 
   goToDetails(recipe: any) {
-    let startIndex = recipe.self.indexOf('/v2/') + 4;
-    let endIndex = recipe.self.indexOf('?');
+    let startIndex = recipe.self.indexOf("/v2/") + 4;
+    let endIndex = recipe.self.indexOf("?");
     let extractedId = recipe.self.substring(startIndex, endIndex);
     console.log(extractedId);
     this.router.navigate(['/recipe', extractedId]);
